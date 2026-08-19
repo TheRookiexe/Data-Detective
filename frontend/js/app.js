0fd5f7f13125470f1c80057a3244572a255fa31f
TheRookiexe <mishrxa36@gmail.com>
2026-08-19 17:36:54 +0000

    Build initial frontend layout

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,9 @@
 console.log("Data Detective frontend loaded")
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+item.addEventListener('click', function(e) {
+    document.querySelector('.menu-item.active')?.classList.remove('active');
+    this.classList.add('active');
+});
+});
