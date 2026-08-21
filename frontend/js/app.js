a92613f1acbd5a7e88af0b6d69ff92f066386531
TheRookiexe <mishrxa36@gmail.com>
2026-08-21 17:36:04 +0000

    connect frontend dataset upload to analysis API

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,18 +3,20 @@ const uploadBtn = document.getElementById('upload-btn');
 const fileInput = document.getElementById('real-file-input');
 const fileNameDisplay = document.getElementById('selected-file-name');
 const fileDelBtn = document.getElementById('delete-file-btn');
-
+const submitBtn = document.getElementById('submit-btn');
 
 uploadBtn.addEventListener('click', function(){
     fileInput.click();
 });
 
-fileInput.addEventListener('change', function(){
+fileInput.addEventListener('change', function(e){
     if (fileInput.files.length>0){
+        e.stopPropagation();
         console.log(fileInput.files);
         fileNameDisplay.textContent = ' Selected: '+fileInput.files[0].name;
         fileNameDisplay.style.display = 'block';
         fileDelBtn.style.display = 'block';
+        submitBtn.style.display = 'inline-flex'
 
     } else {
         resetFileSection();
@@ -26,11 +28,54 @@ fileDelBtn.addEventListener('click', function(e){
     resetFileSection();
 })
 
+let analysisResult = null;
+submitBtn.addEventListener('click', async(e)=>{
+    e.stopPropagation();
+    if (fileInput.files.length === 0) {
+        return;
+    }
+    submitBtn.disabled = true;
+    const formInput = new FormData();
+    formInput.append('file', fileInput.files[0])
+    submitBtn.textContent = "ANALYZING...";
+    try {
+        const response = await fetch("/api/analyze", {
+            method: "POST",
+            body: formInput
+        });
+
+        if (!response.ok) {
+            throw new Error(`Http Error: ${response.status}`);
+        }
+
+        const result = await response.json();
+        analysisResult = result;
+
+        console.log("this is response:");
+        console.log(result);
+
+        submitBtn.textContent = "ANALYZED";
+
+    } catch (error) {
+
+        console.error(error);
+        submitBtn.textContent = "FAILED";
+
+    } finally {
+
+        submitBtn.disabled = false;
+
+    }
+    
+});
+
+// helper functions
 function resetFileSection(){
     fileInput.value = '';
     fileNameDisplay.textContent = '';
     fileDelBtn.style.display = 'none';
     fileNameDisplay.style.display = 'none';
+    submitBtn.style.display = 'none';
 }
 
 
